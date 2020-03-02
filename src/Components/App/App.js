457f40aefafb1e4fd4dec33e1b1b67ea62036313
Luke Peña <luketpena@gmail.com>
2020-03-02 16:38:34 -0600

    Added greeting based on the time of day

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,16 +1,43 @@
 import React from 'react';
 import './App.css';
+
+//-----< Data Imports >-----\\
 import Companies from '../../Data/Companies';
 import Guests from '../../Data/Guests';
 
-function App() {
+export default function App() {
+
+  //>> Constructor for Message
+  function Message(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+
+    //Generates a greeting based on the time of day
+    this.findTime = function() {
+      let time = new Date().getHours();
+      if (time<12) {
+        return 'Good morning';
+      } else if (time<5) {
+        return 'Good afternoon';
+      } else if (time<8) {
+        return 'Good evening';
+      } else {
+        return 'Good night'
+      }
+    }
+  }
+
+  function generateGreeting() {
+    let newMessage = new Message('Luke','Peña');
+    newMessage.findTime();
+  }
+
   return (
     <div className="App">
       <h1>Guest Greeter</h1>
       {JSON.stringify(Companies)}
       {JSON.stringify(Guests)}
+      <button onClick={generateGreeting}>Generate Greeting</button>
     </div>
   );
 }
-
-export default App;
